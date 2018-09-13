2a2f3d814dd39b02392fd5bf804f806a5416e74f
aditi <aditis.kmit@gmail.com>
2018-09-13 16:13:30 +0530

    add invalid res

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 import users from '../apis/users/index';
 const router = app => {
   app.use("/api", users);
+  app.get('/', (req, res) => {
+    res.send('Invalid endpoint!');
+});
 };
 export default router;
